70b10dd5f17a5ce9f8ddc51b95e27218f3a96fe2
설연수 <seolnavy@naver.com>
2024-02-25 15:05:43 +0900

    Update notify_prs.py

--- .github/workflows/notify_prs.py
+++ .github/workflows/notify_prs.py
@@ -16,14 +16,27 @@ def notify_prs(slack_webhook_url, repo_owner, repo_name):
 
         pr_messages.append(f"*PR 제목:* {pr_link}\n*작성자:* {pr['user']['login']}\n*등록일로부터 경과 일수:* {days_since_creation}일\n---")
 
-    return {
-        'text': f"리뷰가 필요한 PR이 {len(unreviewed_prs)}개 있습니다.\n\n{'\n'.join(pr_messages)}",
+    message_text = f"리뷰가 필요한 PR이 {len(unreviewed_prs)}개 있습니다.\n\n{'\n'.join(pr_messages)}"
+
+    send_slack_notification(slack_webhook_url, message_text)
+
+def send_slack_notification(webhook_url, message_text):
+    payload = {
+        'text': message_text
     }
+
+    print(message_text)
+
+    # response = requests.post(webhook_url, json=payload)
+
+    # if response.status_code == 200:
+    #     print("Slack 알림이 성공적으로 전송되었습니다.")
+    # else:
+    #     print(f"Slack 알림 전송 실패. 상태 코드: {response.status_code}, 응답 내용: {response.text}")
 
 if __name__ == '__main__':
     slack_webhook_url = os.environ.get('SLACK_WEBHOOK_URL')
     repo_owner = os.environ.get('REPOSITORY_OWNER')
     repo_name = os.environ.get('REPOSITORY_NAME')
 
-    message = notify_prs(slack_webhook_url, repo_owner, repo_name)
-    print(message['text'])
+    notify_prs(slack_webhook_url, repo_owner, repo_name)
